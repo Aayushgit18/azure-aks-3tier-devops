65704f77721aa88801b83768413b13cdc5349a69
Aayushgit18 <aayushchoundiye@gmail.com>
2026-01-21 15:19:03 +0530

    Update api.service.ts

--- AngularCRUD/src/app/services/api.service.ts
+++ AngularCRUD/src/app/services/api.service.ts
@@ -3,13 +3,7 @@ import { Injectable } from '@angular/core';
 
 @Injectable({ providedIn: 'root' })
 export class ApiService {
-<<<<<<< HEAD
   baseUrl = '/api';
-=======
-
-  // Kubernetes service name + port
-  private baseUrl: string = 'http://52.224.87.132:30081';
->>>>>>> e30ad0327d633ca43c5e59ac6f9c993bd6f60910
 
   constructor(private http: HttpClient) {}
 
@@ -29,5 +23,3 @@ export class ApiService {
     return this.http.delete<any>(`${this.baseUrl}/deleteBook/${bookId}`);
   }
 }
-
-
